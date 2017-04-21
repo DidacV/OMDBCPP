d6a3db0259616a98aced44197ebf3fb237198e9b
Didac Viteri <Didac@Diegos-MacBook-Pro.local>
2017-04-21 22:55:38 +0100

    Added tasks

--- src/main.cpp
+++ src/main.cpp
@@ -36,9 +36,77 @@ int main(int argc, char** argv) {
     //cout << *db_ptr;
     //auto l = [](Rating const &r, Rating const &_r){ return (r < _r);};
 
-    auto l = [](Movie const &r, Movie const &_r){ return (r.get_title().size() > _r.get_title().size());};
+    cout<<"//*************************************************************//"<<endl;
+    cout<<"//**************             Task 1           *****************//"<<endl;
+    cout<<"//*************************************************************//"<<endl; 
+    // sort_mdb accepts a custom comparison to change the sorting  
+    // E.g: auto comp = [](Movie const &m, Movie const &_m)        
+    //                    {return (m.get_date() > _m.get_date());}
+    //db_ptr->sort_mdb(comp);
+    db_ptr->sort_mdb(); 
+    cout << *db_ptr << endl;
 
-    cout << db_ptr->retrieve(l);
+
+    cout<<"//*************************************************************//"<<endl;
+    cout<<"//**************             Task 2           *****************//"<<endl;
+    cout<<"//*************************************************************//"<<endl;
+    // retrieve accepts a criteria, a custom comparison (optional)
+    // and a position (optional) 
+    auto criteria_t2 = [](Movie *m)
+	               {return (m->get_genres().find("Film-Noir") != string::npos);};
+    auto comparison_t2 = [](Movie const &m, Movie const &_m)
+	                 {return (m.get_duration() > _m.get_duration());};
+    int pos_t2 = 2; // Third (0 index)
+
+    cout << db_ptr->retrieve(pos_t2, comparison_t2, criteria_t2) << endl;
+
+    
+    cout<<"//*************************************************************//"<<endl;
+    cout<<"//**************             Task 3           *****************//"<<endl;
+    cout<<"//*************************************************************//"<<endl;
+    auto criteria_t3 = [](Movie *m)
+	               {return (m->get_genres().find("Sci-Fi") != string::npos);};
+    auto comparison_t3 = [](Movie const &m, Movie const &_m)
+	               {return (m.get_avg_rating() > _m.get_avg_rating());};
+    int pos_t3 = 9; // Tenth (0 index)
+    
+    cout << db_ptr->retrieve(pos_t3, comparison_t3, criteria_t3) << endl;
+
+    
+    cout<<"//*************************************************************//"<<endl;
+    cout<<"//**************             Task 4           *****************//"<<endl;
+    cout<<"//*************************************************************//"<<endl;
+    auto comparison_t4 = [](Movie const &m, Movie const &_m)
+	               {return (m.get_avg_rating() > _m.get_avg_rating());};
+    //int pos_t3; // 
+
+    cout << db_ptr->retrieve(0, comparison_t4) << endl;
+
+    
+    cout<<"//*************************************************************//"<<endl;
+    cout<<"//**************             Task 5           *****************//"<<endl;
+    cout<<"//*************************************************************//"<<endl;
+    auto comparison_t5 = [](Movie const &m, Movie const &_m)
+	                 {return (m.get_title().size() > _m.get_title().size());};
+
+    cout << db_ptr->retrieve(0, comparison_t5) << endl;
+
+    cout<<"//*************************************************************//"<<endl;
+    cout<<"//**************             Task 6           *****************//"<<endl;
+    cout<<"//*************************************************************//"<<endl;
+    auto comparison_t6 = [](Rating const &r, Rating const &_r)
+	                 {return (r < _r);};
+
+    cout << r_ptr->retrieve(100, comparison_t6) << endl;
+    /****************************************/
+    /****             Task 6           ******/
+    /****************************************/
+    auto comp = [](Movie const &r, Movie const &_r){ return (r.get_duration() > _r.get_duration());};
+
+    //auto criteria = [](Movie *m){return (m->get_genres().find("Sci-Fi")!=string::npos);};
+
+    auto criteria = [](Movie *m){return (m->get_date() == 1984);};
+    //cout << db_ptr->retrieve(criteria);
     //r_ptr->retrieve(l);
     
     //Rating r = r_ptr->retrieve([](Rating &r, Rating &_r){ return (r < _r);});
